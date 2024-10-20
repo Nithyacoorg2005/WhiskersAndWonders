783412087563ded14dddb5a2c38b903946ec00c8
NithyashreeCS <nityaatria99@gmail.com>
2024-10-20 22:26:22 +0530

    initial channges

---
+++ src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+// import ReactDOM from 'react-dom/client';
+import React from 'react';
+import { createRoot } from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css'; // This imports your Tailwind CSS
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
